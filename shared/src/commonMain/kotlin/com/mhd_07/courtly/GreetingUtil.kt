207ef9ab3ca3abf46b176182448f8838a9347a76
Mohammed Hazem <mo.hazem2007@gmail.com>
2026-07-14 18:01:34 +0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/mhd_07/courtly/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.mhd_07.courtly
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/mhd_07/courtly/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.mhd_07.courtly
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
